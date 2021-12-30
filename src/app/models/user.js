b01f16c3a2173c80f1488f613dca6446e5c59e08
sancode86 <santiagosuarez86@gmail.com>
2021-12-30 20:59:32 -0300

    ss

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -23,9 +23,7 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.generateHash = function (password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
-
 userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
-
 module.exports = mongoose.model("User", userSchema);
